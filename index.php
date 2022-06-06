21a780ef224fbd02ae1184da9e6f4699d4f2fc4c
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-06-06 15:11:43 +0200

    start

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$paragrafo = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, voluptatum culpa! Ratione nemo, voluptatum obcaecati omnis nihil nobis similique perspiciatis laborum recusandae, minima non. Error aperiam numquam ipsum recusandae Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati accusamus debitis culpa dicta dolor delectus maiores illo corrupti aspernatur sint, eveniet fuga labore natus ipsum et harum doloremque exercitationem esse?"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP-BADWORDS</title>
+</head>
+<body>
+  <p><?php echo $paragrafo ?></p>
+</body>
+</html>
